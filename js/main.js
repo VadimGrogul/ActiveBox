3fb795d40a1a9a65abc826da07daa8a2ddbf8850
Vadim <grogulvadim@gmail.com>
2019-03-01 16:44:26 +0200

    'ready'

--- js/main.js
+++ js/main.js
@@ -1,8 +1,58 @@
-let adaptMenuBtn = $('.header__menu-open');
-let adaptMenu = $('.header__adapt-nav');
-let a = $('[data-isOpen]');
 
-$('.header__menu-open').click(() => {
-    $('.header__adapt-nav').slideToggle('slow');
+
+$(window).on('scroll', function(){
+    if($(this).scrollTop() > 500) {
+        $('.header').css({'background-color' : 'rgba(58, 62, 100, 0.9)'})
+    } else {
+        $('.header').css({'background-color' : 'transparent'})
+    } 
 })
 
+let menu = $('.header__adapt-nav').attr('data-isOpen');
+
+
+$('.header__menu-open').click(() => {
+    if(menu == 0) {
+        $('.header__adapt-nav').css({'height' : '22px', 'paddingTop' : '2rem'});
+        menu = 1;
+    } else {
+        $('.header__adapt-nav').css({'height' : '0', 'paddingTop' : '0'});
+        menu = 0;
+    }
+})
+
+let currentImageIndex = 0;
+let images = ['./img/girl.png', './img/a.jpg', './img/b.jpg', './img/c.jpg', './img/d.jpg'];
+let imgBlock = $('.clients__photo');
+
+    for(let i = 0; i < images.length; i++) {
+        imgBlock.eq(i).css('backgroundImage', 'url(' + images[i] + ')');
+    }
+
+    $('.slider-wrap').eq(currentImageIndex).css('opacity', '1');
+
+function autoSlider() {
+    
+    $('.slider-wrap').eq(currentImageIndex).css('opacity', '0');
+    $('.clients-slider__circles-item').eq(currentImageIndex).removeClass('clients-slider__circles-item--active');
+    
+    currentImageIndex += 1;
+    
+    if(currentImageIndex == $('.slider-wrap').length) {
+        currentImageIndex = 0;   
+    }
+
+    $('.clients-slider__circles-item').eq(currentImageIndex).addClass('clients-slider__circles-item--active');
+
+    $('.slider-wrap').eq(currentImageIndex).css('opacity', '1');
+    
+    
+}
+
+
+
+    
+setInterval(autoSlider, 3000)
+
+
+console.log($('.slider-wrap').length);
